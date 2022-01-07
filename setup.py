b47809db34e847460f317cbbe6426c726be6407a
NWeis97 <nicolai.weisbjerg@gmail.com>
2022-01-07 09:59:41 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is the exam projectt of group 20 for Machine Learning Operation at DTU.',
+    author='Group 20',
+    license='MIT',
+)
