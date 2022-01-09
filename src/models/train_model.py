8ed90a93561e43d8f5dbbe607bf9363537f793a1
NWeis97 <nicolai.weisbjerg@gmail.com>
2022-01-09 12:21:23 +0100

    extra

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -59,29 +59,6 @@ def main():
     ################### Load model ####################
     ###################################################
 
-    # Get model configuration.
-    print('Loading configuraiton...')
-    model_config = GPT2Config.from_pretrained(pretrained_model_name_or_path=model_name, num_labels=n_labels)
-
-    # Get model's tokenizer.
-    print('Loading tokenizer...')
-    tokenizer = GPT2Tokenizer.from_pretrained(pretrained_model_name_or_path=model_name)
-    # default to left padding
-    tokenizer.padding_side = "left"   #NB: Needs better understanding
-    # Define PAD Token = EOS Token = 50256
-    tokenizer.pad_token = tokenizer.eos_token  #NB: Needs better understanding
-
-
-    # Get the actual model.
-    print('Loading model...')
-    model = GPT2ForSequenceClassification.from_pretrained(pretrained_model_name_or_path=model_name_or_path, config=model_config)
-
-    # resize model embedding to match new tokenizer
-    model.resize_token_embeddings(len(tokenizer))
-
-    # fix model padding token id
-    model.config.pad_token_id = model.config.eos_token_id
-
     # Load model to defined device.
     model.to(device)
     print('Model loaded to `%s`'%device)
